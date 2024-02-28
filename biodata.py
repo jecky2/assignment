5162cb71a09e9ba24e7f51493b6131f0740f3ed2
jecky2 <161566708+jecky2@users.noreply.github.com>
2024-02-28 14:56:58 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,11 @@
+name = "jecky evanglis aritonang"
+age = "19"
+live = "cibolang"
+hobby = "sleeping"
+gender = "male"
+
+print("my name is =" ,name)
+print("my age =" ,age)
+print("i live in =" ,live)
+print("my hoby =" ,hobby)
+print("gender =" ,gender)
